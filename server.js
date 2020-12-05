71ebf09d2be6998fa982c09a3a45fd33ba51b7b4
Hung Dinh <dinhhung2910@gmail.com>
2020-12-05 19:53:52 +0700

    * fix cors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const {parseFromFile} = require('./utils/generateInput');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
@@ -21,6 +22,7 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 // Init Middleware
 app.use(express.json({extended: false}));
+app.use(cors());
 app.use(function(req, res, next) {
   // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Origin', '*');
